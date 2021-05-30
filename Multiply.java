da8e85dc4ce5b3d850114c0321e9c3890ac8a36f
Anuj Kumar Sharma <anuj55149@gmail.com>
2021-05-31 02:22:43 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+
+        System.out.println("the product is " + (a*b));
+    }
+
+}
